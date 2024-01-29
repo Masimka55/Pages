592ce3fe63a255beedb135fe5292611a55f0cc81
Masimka55 <146752851+Masimka55@users.noreply.github.com>
2024-01-29 19:04:13 +0400

    Delete moduleJS directory

--- moduleJS/creatsElement.js
+++
@@ -1,6 +0,0 @@
-export function createElement(tag, className, city) {
-       let element = document.createElement(tag);
-       element.classList.add(className);
-       element.textContent = city;
-       return element;
-}
--- moduleJS/styleHeart.js
+++
@@ -1,8 +0,0 @@
-import { FAVORITE } from "./elements.js"
-export function styleHeartNone(){
-    FAVORITE.BTM_HEART.style.fill = ''
-  }
-
-export function styleHeartRed(){
-    FAVORITE.BTM_HEART.style.fill = 'red'
-}
